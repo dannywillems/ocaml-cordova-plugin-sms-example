6a2ce3ce373f696a82236527083ceca3522e3fa7
Danny Willems <contact@danny-willems.be>
2016-06-17 17:11:36 +0200

    Use functions instead of objects

--- app/ml/test.ml
+++ app/ml/test.ml
@@ -1,18 +1,15 @@
 let on_device_ready () =
-  let s = Cordova_sms.t () in
-  let dialog = Cordova_dialogs.t () in
-
   let num_node = Jsoo_lib.get_input_by_id "num" in
   let msg_node = Jsoo_lib.get_input_by_id "msg" in
   let btn_node = Jsoo_lib.get_button_by_id "submit" in
 
   let succ () =
-    dialog#alert "Message sent!" ~title:"It's working!" ();
+    Cordova_dialogs.alert "Message sent!" ~title:"It's working!" ();
     num_node##.value := (Js.string "");
     msg_node##.value := (Js.string "")
   in
   let err msg =
-    dialog#alert msg ~title:"Something wrong =(:" ()
+    Cordova_dialogs.alert msg ~title:"Something wrong =(:" ()
   in
 
   btn_node##.onclick := Dom.handler
@@ -21,11 +18,11 @@ let on_device_ready () =
       let num = Js.to_string (num_node##.value) in
       let msg = Js.to_string (msg_node##.value) in
       if num = "" then
-        dialog#alert "Please enter a phone number." ~title:"Missing field" ()
+        Cordova_dialogs.alert "Please enter a phone number." ~title:"Missing field" ()
       else if msg = "" then
-        dialog#alert "Please enter a message." ~title:"Missing field" ()
+        Cordova_dialogs.alert "Please enter a message." ~title:"Missing field" ()
       else
-        s#send ~num:num ~msg:msg ~succ_cb:succ ~err_cb:err ();
+        Cordova_sms.send ~num:num ~msg:msg ~succ_cb:succ ~err_cb:err ();
       Js._false
   )
 
@@ -48,4 +45,5 @@ let on_device_ready () =
   );
   *)
 
-let _ = Cordova.Event.device_ready on_device_ready
+let _ =
+  Cordova.Event.device_ready on_device_ready
